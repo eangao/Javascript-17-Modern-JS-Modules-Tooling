843197af981fd575493769bf1e51b2156d4777ad
eangao <elmarcera@gmail.com>
2023-12-29 15:26:49 +0800

    An Overview of Modules in JavaScript

--- script.js
+++ script.js
@@ -33,3 +33,27 @@
 // For example, the popular React framework or jQuery,
 // or even the Leaflet library,
 // that we used before in our Mapty project.
+
+///////////////////////////////////////////////////////////////
+// An Overview of Modules in JavaScript
+///////////////////////////////////////////////////////////////
+//SEE PDF LECTURE AND VIDEO
+
+// Modules are a super important part
+// of software development.
+// And so in this lecture,
+// we're gonna talk about modules in more depth
+// and learn how they work behind the scenes.
+// So essentially, a module
+// is a reusable piece of code
+// that encapsulates implementation details
+// of a certain part of our project.
+// Now that sounds a bit like a function
+// or even a class,
+// but the difference is that a module
+// is usually a standalone file.
+// Now that's not always the case,
+// but normally when we think of a module
+// we think of a separate file.
+// So of course a module always contains some code
+// but it can also have imports and exports.
