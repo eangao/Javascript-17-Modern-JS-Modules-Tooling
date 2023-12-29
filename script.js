2a53dc265bfc1f0de79c8c96667fcb0f4dba5470
eangao <elmarcera@gmail.com>
2023-12-29 11:58:17 +0800

    An Overview of Modern JavaScript Development

---
+++ script.js
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////
+// An Overview of Modern JavaScript Development
+///////////////////////////////////////////////////////
+//SEE PDF LECTURE AND VIDEO
+
+// Let's start a section with a general overview
+// of Modern JavaScript Development.
+// So basically of how we write JavaScript today.
+
+// Because when we built applications,
+// we don't just write all of our code into one big script
+// send that script as it is to the browser,
+// and call it a day.
+// Now it used to be like this,
+// but the way we built JavaScript applications,
+// has changed tremendously over the last couple of years.
+
+// So back in the day,
+// we used to write all our codes
+// into one big script or maybe multiple scripts.
+// But today, we divide our projects into multiple modules
+// and these modules can share data between them
+// and make our code more organized and maintainable.
+
+// Now, one great thing about modules
+// is that we can also include 3rd-party modules
+// into our own code.
+// And there are thousands of open source modules,
+// which we also call packages,
+// that developers share on the NPM repository.
+
+// And we can then use these packages for free in our own code.
+// For example, the popular React framework or jQuery,
+// or even the Leaflet library,
+// that we used before in our Mapty project.
